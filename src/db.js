0475aafeee4f54cba0b00160591dddaebae91fb1
storyofmicrobes <storyofmicrobes@gmail.com>
2026-06-13 09:47:14 +0530

    Delete src/db.js

--- src/db.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const Database = require('better-sqlite3');
-const dbPath = process.env.DATABASE_PATH || path.join(__dirname, '..', 'data', 'geneprospera_lms.db');
-const db = new Database(dbPath);
-db.pragma('journal_mode = WAL');
-db.pragma('foreign_keys = ON');
-module.exports = db;
